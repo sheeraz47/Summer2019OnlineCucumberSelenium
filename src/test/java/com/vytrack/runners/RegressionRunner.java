d048b4526752bb032d56dc041efc0f881b4961f4
sheeraz47 <sheeraz47@gmail.com>
2020-01-19 17:23:53 -0500

    updated cukes runner

--- src/test/java/com/vytrack/runners/RegressionRunner.java
+++ src/test/java/com/vytrack/runners/RegressionRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {
                 "src/test/resources/features/activities",
-                "src/test/resources/features/fleet"
+                "src/test/resources/features/fleet",
+                "src/test/resources/features/login"
         },
         glue = "com/vytrack/step_definitions",
         plugin = {"html:target/default-cucumber-reports",
